a537ea6fcc72287b4825ba8afe8d9717542e6a97
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-28 11:29:24 -0500

    feat: init commit

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+import { StarOutlined } from '@ant-design/icons';
+import {Card} from 'antd';
+import Meta from 'antd/es/card/Meta';
+
+const PokemonCard = ({name}) => {
+    return <Card 
+
+    title={name}
+    cover={<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' alt='Ditto' />}
+    extra={<StarOutlined />}
+   >
+    <Meta description='fire,magic'/>
+    </Card>
+}
+export default PokemonCard 
