749a9f59244eb53c49e9d966f8eea439c7dcbba5
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-28 11:31:52 -0500

    feat: init commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { Col } from 'antd'
+import Searcher from './components/Searcher'
+import PokemonList from './components/PokemonList'
+import logo from './statics/logo.svg'
+import { useEffect, useState } from 'react'
+import {getPokemon} from './api'
+function App() {
+  const [pokemons, setPokemons] = useState([])
+  useEffect(() => {
+    const fetchPokemons = async () => {
+     const pokemonsRes = await getPokemon();
+     setPokemons(pokemonsRes);
+    }
+    fetchPokemons();
+  },
+    [])
+
+  return (
+    <div className="App">
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux' />
+      </Col>
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+      <PokemonList pokemons={pokemons} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Searcher.jsx
@@ -0,0 +1,6 @@
+import {Input} from 'antd';
+
+const Searcher = () => {
+    return <Input.Search placeholder='Buscar...' />
+}
+export default Searcher
